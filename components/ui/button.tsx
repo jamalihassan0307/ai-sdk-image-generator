77bdbc296d8813ee3fe9286eeb7daf536af8b0a2
jamalihassan0307 <jamalihassan0307@gmail.com>
2025-02-15 20:29:12 +0500

    ui upgration

--- components/ui/button.tsx
+++ components/ui/button.tsx
@@ -19,8 +19,7 @@ const buttonVariants = cva(
           "bg-secondary text-secondary-foreground shadow-sm hover:bg-secondary/80",
         ghost: "hover:bg-accent hover:text-accent-foreground",
         inactive: "",
-        active:
-          "border border-input bg-background shadow-sm",
+        active: "border border-input bg-background shadow-sm",
         link: "text-primary underline-offset-4 hover:underline",
       },
       size: {
